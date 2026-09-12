592bdf7e75fff73ce9ac8ab5246127cb2462a65f
camiAFG <camilaafanador12@gmail.com>
2026-09-11 20:16:53 -0500

    :sparkles: terminamos el tercer ejercicio

--- Lista de estudiantes.py
+++ Lista de estudiantes.py
@@ -1,7 +1,6 @@
 estudiantes = ["Juan","Ana","Pedro","Laura"]
 print(estudiantes[0])
 print(estudiantes[-1])
-print(estudiantes[-4])
 estudiantes[2] = "Carlos"
 estudiantes.append("Sofia")
 estudiantes.remove("Juan")
---
+++ Recorrer una lista.py
@@ -0,0 +1,9 @@
+numeros = [10,200,15,40,50]
+for numero in numeros:
+    print(numero)
+
+for n in numeros:
+    if n > 25:
+        print(f"{n} Es mayor que 25")
+    else:
+        print(f"{n} Es menor que 25")
