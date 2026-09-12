906a45e49ae716ae0e8b46cd44655417f711459a
camiAFG <camilaafanador12@gmail.com>
2026-09-11 19:59:48 -0500

    :sparkles: terminamos los dos primeros ejercicios

---
+++ Lista de compras.py
@@ -0,0 +1,17 @@
+##EJERCICIO 1
+
+compra = []
+compra.append("pan")
+compra.append("leche")
+compra.append("huevos")
+print(compra)
+compra.remove("leche")
+print(compra)
+
+compra2 = []
+compra2.append("pan")
+compra2.append("leche")
+compra2.append("huevos")
+print(compra2)
+compra2.pop(1)
+print(compra2)
---
+++ Lista de estudiantes.py
@@ -0,0 +1,8 @@
+estudiantes = ["Juan","Ana","Pedro","Laura"]
+print(estudiantes[0])
+print(estudiantes[-1])
+print(estudiantes[-4])
+estudiantes[2] = "Carlos"
+estudiantes.append("Sofia")
+estudiantes.remove("Juan")
+print(estudiantes)
